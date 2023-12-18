281eb226d37cad1794d55562d8e86931c5e92b8f
JunYoungKim-kjy <firstkjy1123@gmail.com>
2023-12-18 18:42:40 +0900

    Update Player.java

--- rpg1_1/src/rpg/Player.java
+++ rpg1_1/src/rpg/Player.java
@@ -1,5 +1,18 @@
 package rpg;
 
 public class Player {
-
+	static private int money;
+	static private Guild guild = new Guild();
+	static private Inventory inven = new Inventory();
+	static private Player instance = new Player();
+	Player (){
+		money = 100000;
+		guild.setGuild();
+	}
+	public static void setMoney(int money) {
+		Player.money = money;
+	}
+	public static int getMoney() {
+		return money;
+	}
 }
